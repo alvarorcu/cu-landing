b50505273d23af0dae133e6aabe9b79e636ce02a
alvarorcu <aconchauriol@gmail.com>
2014-12-05 10:12:22 -0500

    se arreglo el loader en las nuevas paginas

---
+++ js/packs.js
@@ -0,0 +1,6 @@
+/* Page Loader : hide loader when all are loaded */
+$(window).load(function(){
+    $('.page-loader').addClass('hidden');
+});
+
+/* END OF Page Loader : hide loader when all are loaded */
