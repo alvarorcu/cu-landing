55ede7cc1629059c8989c87d688e0ebb35198c7c
Diego Berrocal <cestdiego@gmail.com>
2014-12-10 03:09:03 -0500

    Activation Codes generated per email basis

--- js/activation_code.js
+++ js/activation_code.js
@@ -23,54 +23,77 @@ var paymentMethods = {
     deposit: guids.child('deposit'),
     manual: guids.child('manual')
 };
-function pushPaymentMethod(payment_method, creator){
+
+var admins = [
+    "758637690840380",   // Nohelia
+    "863811930325042",   // Cristhian
+    "10205290908290843", // Diego
+    "10152916118601052", // Alvaro
+    "331359500383553"    // Hans
+];
+
+function pushPaymentMethod(payment_method, creator, pulpi_email){
     var firebase_ref = paymentMethods[payment_method];
     var now = new Date();
     var result  = {
         used: false,
         creator: creator,
+        pulpi_email: pulpi_email,
         createdAt: now.getDate() + '/' + now.getMonth() + '/' + now.getFullYear() +" " + now.getHours() + ":" + now.getMinutes()
     };
     var new_push = firebase_ref.push(result);
-    console.log(new_push.key());
+    return new_push.key();
 }
 ref.onAuth(function(authData) {
     console.log( authData );
     if (authData) {
         // user authenticated with Firebase
         var fullName = findFullName(authData);
-        
         document.querySelector('.avatar img')
             .setAttribute("src", findProfilePic(authData));
-        document.querySelector('.activation_codes')
-            .classList.remove("hideThis");
-        document.querySelector('.not_permitted')
-            .classList.add("hideThis");
+        
+        if (authData.provider == "facebook" && admins.indexOf(authData.facebook.id) != -1  ){
+            document.querySelector('.activation_codes')
+                .classList.remove("hideThis");
+            document.querySelector('.not_permitted')
+                .classList.add("hideThis");
 
-        $('.activation_codes .button')
-            .each(function(key, value){
-                console.log(value);
-                var payment_method = $(value).attr('id');
-                var creator = fullName;
+            $('.activation_codes .button')
+                .each(function(key, value){
+                    console.log(value);
+                    var payment_method = $(value).attr('id');
+                    var creator = fullName;
                 
-                value.addEventListener('click', function(){
-                    pushPaymentMethod(payment_method, creator);
+                    value.addEventListener('click', function(){
+                        var pulpi_email = $('input[name=pulpi_email]').val();
+                        if(pulpi_email){
+                            var key = pushPaymentMethod(payment_method, creator, pulpi_email);
+                            $('#copy-code').append("<h2>" + key.substring(1,key.length) + "   email: " + pulpi_email + "</h2>");
+                        }
+                        else{
+                            alert("NO puede estar vacio");
+                        }
+                    });
                 });
-            });
-        // .addEventListener('click', function(){
-        //     console.log("Me cliclearon el activation code");
-        // });
+            // .addEventListener('click', function(){
+            //     console.log("Me cliclearon el activation code");
+            // });
         
 
-        // console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
-    } else {
-        document.querySelector('.avatar img')
-            .setAttribute("src",
-                          "../img/pict/avatar.jpg");
+            // console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
+        }
+    else{
         document.querySelector('.activation_codes').classList.add("hideThis");
         document.querySelector('.not_permitted').classList.remove("hideThis");
     }
-});
+} else {
+    document.querySelector('.avatar img')
+        .setAttribute("src",
+                      "../img/pict/avatar.jpg");
+    document.querySelector('.activation_codes').classList.add("hideThis");
+    document.querySelector('.not_permitted').classList.remove("hideThis");
+}
+          });
 
 function userLogin(Provider){
     ref.authWithOAuthRedirect(Provider, function(err, authData){
